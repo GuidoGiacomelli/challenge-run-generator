0774d71740532d23aa9a08e5af0dfa06ee1a507a
guido <guidogiacomelli3@gmail.com>
2026-06-01 12:18:55 -0300

    - Added a function to let the user save their own challenges and games to JSON file. - Added a simple CLI menu to choose an action.

--- challenge_run_gen.py
+++ challenge_run_gen.py
@@ -1,5 +1,6 @@
 import random
 import json
+from unittest import case
 with open("games.json", "r") as file:
     games = json.load(file)
 
@@ -11,21 +12,38 @@ def display_games(games):
 def get_challenge(games, game_name):
     return random.choice(games[game_name])
 
+def add_challenge(games, game_name, challenge):
+    if game_name not in games:
+        games[game_name] = []
+    games[game_name].append(challenge)
+    with open("games.json", "w") as file:
+        json.dump(games, file, indent=4)
+    print("Challenge added successfully.")
+
 while True:
-    print("Challenge run generator")
-    display_games(games)
-    game_name = input("Select a game: ")
-    game_name = game_name.lower()
-    if game_name in games:
-        print(f"Your challenge: {get_challenge(games, game_name)}")
-    else:
-        print("Game is not available yet")
-    while True:
-        choice = input("Do you want to continue? (y/n)")
-        if choice.lower() == "y":
-            break
-        elif choice.lower() == "n":
+    print("Challenge run generator\n1. Show game list\n2. Get a challenge\n3. Add a challenge\n4. Exit")
+    match input("Select an option: "):
+        case "1":
+            display_games(games)
+        case "2": 
+            game_name = input("Select a game: ").lower()
+            if game_name in games:
+                print(f"Your challenge: {get_challenge(games, game_name)}")
+            else:
+                print("Game is not available yet")
+            while True:
+                choice = input("Do you want to continue? (y/n)")
+                if choice.lower() == "y":
+                    break
+                elif choice.lower() == "n":
+                    print("Process finished")
+                    exit()
+                else:
+                    print("Invalid input. Try again")
+        case "3":
+            game_name = input("Select a game: ").lower()
+            challenge = input("Enter a new challenge: ")
+            add_challenge(games, game_name, challenge)
+        case "4":
             print("Process finished")
             exit()
-        else:
-            print("Invalid input. Try again")
